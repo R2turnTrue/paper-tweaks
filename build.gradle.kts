dbfb371f0f2eb474cb52652c33a4720ed9b4ffab
True <johanbag290@gmail.com>
2021-09-12 21:18:48 +0900

    First commit!

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,6 +17,7 @@ repositories {
 
 dependencies {
     compileOnly("io.papermc.paper:paper-api:1.17.1-R0.1-SNAPSHOT")
+    implementation("net.projecttl:InventoryGUI-api:4.1.3")
 
     implementation(kotlin("stdlib"))
 
---
+++ src/main/kotlin/xyz/r2turntrue/papertweaks/plugin/SamplePlugin.kt
@@ -0,0 +1,12 @@
+package xyz.r2turntrue.papertweaks.plugin
+
+import org.bukkit.plugin.java.JavaPlugin
+
+/**
+ * @author R2turnTrue
+ */
+class SamplePlugin : JavaPlugin() {
+
+
+
+}
